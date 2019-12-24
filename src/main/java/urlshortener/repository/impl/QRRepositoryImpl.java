ed219c7435ae4de96972c82a67409390ab53e6ab
DaniRevillo <revillo16@gmail.com>
2019-12-24 13:55:35 +0100

    Añadido logging del cacheo por consola

--- src/main/java/urlshortener/repository/impl/QRRepositoryImpl.java
+++ src/main/java/urlshortener/repository/impl/QRRepositoryImpl.java
@@ -11,13 +11,21 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 @Repository
 public class QRRepositoryImpl implements QRRepository {
 
+	private static final Logger LOG = LoggerFactory.getLogger(QRRepositoryImpl.class);
+
     @Override
-    @Cacheable(value = "qrs", key = "#text")
+    @Cacheable(cacheNames = "qrs", key = "#text")
     public 	byte[] getQR(String text, int width, int height) {
         try {
+
+			LOG.info("Saving qr with ehcache with hash {} ",text);
 			QRCodeWriter qrCodeWriter = new QRCodeWriter();
 			BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
 			ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
